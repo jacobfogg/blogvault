491ad2999ac333340e332beba7e1b67860d353e6
Jacob Fogg <jacob@copperpress.tech>
2024-03-06 22:56:38 -0700

    init

---
+++ protect/fw/rule/errors.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('ABSPATH') && !defined('MCDATAPATH')) exit;
+
+if (!class_exists('BVProtectRuleError_V547')) :
+class BVProtectRuleError_V547 extends Exception {
+//Root rule error class.
+}
+endif;
